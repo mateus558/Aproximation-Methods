0b9be006ba3e894867852d1ba644f6f1fe8324ca
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2016-11-05 21:15:08 -0200

    Bug fixes on file reading.

--- linear_systems.c
+++ linear_systems.c
@@ -3,6 +3,41 @@
 #include <string.h>
 #include <math.h>
 
+#define EPS 1E-5
+#define MAXIT 1E2
+
+int read_matrix(char *fname, double*** matrix, int *N);
+
+int main(){
+	int N, M, i, j, e;
+	char fname[50], *pos;
+	double** matrix = NULL;
+
+	fgets(fname, sizeof(fname), stdin);
+	if((pos=strchr(fname, '\n')) != NULL)
+    	*pos = '\0';
+	
+	M = read_matrix(fname, &matrix, &N);
+	if(M == -1){
+		printf("The matrix couldn't be read!\n");
+		exit(1);
+	}
+	
+	if(!matrix){
+		printf("The matrix couldn't be read!\n");
+		exit(1);
+	}
+	
+	for(i = 0; i < N; ++i){
+		for(j = 0; j < M; ++j){
+			printf("%lf ", matrix[i][j]);
+		}	
+		printf("\n");
+	}
+	
+	return 0;
+}
+
 int read_matrix(char *fname, double*** matrix, int *N){
 	int i, j, lines, cols, size;
 	double val;
@@ -29,33 +64,3 @@ int read_matrix(char *fname, double*** matrix, int *N){
 	fclose(file);	
 	return cols;
 }
-
-int main(){
-	int N, i, j, e;
-	char fname[50], *pos;
-	double** matrix = NULL;
-
-	fgets(fname, sizeof(fname), stdin);
-	if((pos=strchr(fname, '\n')) != NULL)
-    	*pos = '\0';
-
-	if(read_matrix(fname, &matrix, &N) == -1){
-		printf("The matrix couldn't be read!\n");
-		exit(1);
-	}
-	
-	if(!matrix){
-		printf("The matrix couldn't be read!\n");
-		exit(1);
-	}
-	
-	for(i = 0; i < N; ++i){
-		for(j = 0; j < N+1; ++j){
-			printf("%lf ", matrix[i][j]);
-		}	
-		printf("\n");
-	}
-	
-	return 0;
-}
-
