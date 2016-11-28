5c6d854ea262a033630595f71a951c0cf2689429
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2016-11-28 01:16:42 -0200

    Some bugs fixed

--- linear_systems.c
+++ linear_systems.c
@@ -15,7 +15,6 @@ int relative_error(double* x, double* xk, int N);
 double relative_error_value(double* x, double* xk, int N);
 int line_criterion(double **A, int N);
 double** swap_lines(double **A, int N, int L1, int L2);
-double** swap_columns(double **A, int N, int L1, int L2);
 double LU_decomposition(double **A, double ***L, double ***U, int N);
 double* jacobi_method(double **A, int N, int (*stop_criterion)(double*, double*, int));
 double* seidel_method(double **A, int N, int (*stop_criterion)(double*, double*, int));
@@ -24,7 +23,7 @@ double* back_substitution(double** A, double* b, int N);
 double* LU_solve(double **A, int N);
 
 int main(){
-	int N, M, i, j;
+	int N, M, i, j, o;
 	char fname[3] = {'i', 'n', '\0'}, *pos;
 	double **A = NULL;
 	double *x = NULL;
@@ -46,7 +45,24 @@ int main(){
 	print_matrix(A, N);
 	printf("\n");
 	
-	x = gauss_elimination(A, N);
+	printf("Choose a method [0]Gauss Elimination [1]LU Decomposition [2]Gauss-Jacobi Method [3]Gauss-Seidel Method\n");
+	printf("> ");
+	scanf("%d", &o);
+	
+	switch(o){
+		case 0:
+			x = gauss_elimination(A, N);
+			break;
+		case 1:
+			x = LU_solve(A, N);
+			break;
+		case 2:
+			x = jacobi_method(A, N, relative_error);
+			break;
+		case 3:
+			x = seidel_method(A, N, relative_error);
+			break;
+	}
 	
 	if(x){
 		printf("\nSolution:\n");
@@ -122,21 +138,20 @@ int is_singular(double **A, int N){
 }
 
 int line_criterion(double **A, int N){
-	int i, j, sign = 1;
-	double alpha = 0.0, sum = 0.0, max_alpha = NEG_INF, ratio;
+	int i, j;
+	double alpha = 0.0, sum = 0.0;
 	
 	for(i = 0; i < N; ++i){
-		for(j = 0; j < N; ++j){
-			if(i != j){
-				ratio = A[i][j] / A[i][i];
-				sign = (ratio < 0)?-1:1;
-				alpha += ratio * sign;
-			}
+		for(j = 0; j < i; ++j){
+			sum += fabs(A[i][j]);
 		}
-		if(alpha > max_alpha) max_alpha = alpha;
+		for(j = i+1; j < N; ++j){
+			sum += fabs(A[i][j]);
+		}
+		if(sum < fabs(A[i][i])) return 0;
 	}
 	
-	return (max_alpha < 1)?1:-1;
+	return 1;
 }
 
 double relative_error_value(double* x, double* xk, int N){
@@ -182,21 +197,21 @@ int relative_error(double* x, double* xk, int N){
 double** swap_lines(double **A, int N, int L1, int L2){
 	int i, j, k;
 	int **P = NULL;
-	double sum = 0.0, temp, **res = NULL;
+	double sum = 0.0, temp, *b = NULL, **res = NULL;
 	
 	res = (double **)malloc(N * sizeof(double));
-	for(i = 0; i < N; ++i){ res[i] = (double *)malloc(N+1 * sizeof(double)); }
-	P = (int **)malloc(N * sizeof(int));
-	for(i = 0; i < N; ++i){ P[i] = (int *)malloc(N * sizeof(int)); }
-	for(i = 0; i < N; ++i){
-		for(j = 0; j < i; ++j){
+	for(i = 0; i < N; ++i){ res[i] = (double *)malloc(N * sizeof(double)); }
+	b = malloc(N * sizeof(double));
+	P = malloc(N * sizeof *P); 
+	for(i = 0; i < N; ++i){ 
+		P[i] = malloc(N * sizeof *P[i]);
+		for(j = 0; j < N; ++j)
 			P[i][j] = 0;
-		}
-		res[i][N] = A[i][N];
 		P[i][i] = 1;
-		for(j = i + 1; j < N; ++j){
-			P[i][j] = 0;
-		}
+	}
+	
+	for(i = 0; i < N; ++i){
+		b[i] = A[i][N];
 	}
 	
 	P[L1][L1] = 0;
@@ -215,10 +230,19 @@ double** swap_lines(double **A, int N, int L1, int L2){
     	}
 	}
 	
-	res[L1][N] = A[L2][N];
-	res[L2][N] = A[L1][N];
+	A[L1][N] = b[L2];
+	A[L2][N] = b[L1];
 	
-	return res;
+	for(i = 0; i < N; i++){
+    	for(j = 0; j < N; j++){
+			A[i][j] = res[i][j];
+		}
+		free(res[i]);
+	}	
+	free(res);
+	free(b);
+	
+	return A;
 }
 
 double LU_decomposition(double** A, double ***L, double ***U, int N){
@@ -257,19 +281,46 @@ double LU_decomposition(double** A, double ***L, double ***U, int N){
 }
 
 double* jacobi_method(double **A, int N, int (*stop_criterion)(double*, double*, int)){
-	int i, j, k;
+	int i, j, k, l = -1;
 	double *x = NULL, *xk = NULL, *b = NULL;
 	double s;
 	
 	x = malloc(N * sizeof(double));
 	for(i = 0; i < N; ++i) x[i] = 0.0;
-	
-	if(line_criterion(A, N) == -1) return NULL;
 		
 	xk = malloc(N * sizeof(double));
 	for(i = 0; i < N; ++i) xk[i] = 0.0;
 		
 	b = malloc(N * sizeof(double));
+	
+	for(i = 0; i < N; ++i){
+		for(j = 0; j < i; ++j){
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
+		}
+		if(l != -1)
+			for(j = 0; j < i; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
+		for(j = i+1; j < N; ++j){
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
+		}
+		if(l != -1)
+			for(j = i+1; j < N; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
+		if(l != -1)
+			A = swap_lines(A, N, i, l);
+		l = -1;
+	}
+	
+	printf("\nConditioned Matrix:\n");
+	print_matrix(A, N);
+	
+	if(line_criterion(A, N)){ 
+		printf("\nThis system does not converge.\n");
+		return NULL; 
+	}
+	
 	for(i = 0; i < N; ++i) b[i] = A[i][N];
 	
 	printf("\n");
@@ -322,19 +373,46 @@ double* jacobi_method(double **A, int N, int (*stop_criterion)(double*, double*,
 }
 
 double* seidel_method(double **A, int N, int (*stop_criterion)(double*, double*, int)){
-	int i, j, k;
+	int i, j, k, l = -1;
 	double *x = NULL, *xk = NULL, *b = NULL;
 	double s;
 	
 	x = malloc(N * sizeof(double));
 	for(i = 0; i < N; ++i) x[i] = 0.0;
-	
-	if(line_criterion(A, N) == -1) return NULL;
-	
+		
 	xk = malloc(N * sizeof(double));
 	for(i = 0; i < N; ++i) xk[i] = 0.0;
 		
 	b = malloc(N * sizeof(double));
+	
+	for(i = 0; i < N; ++i){
+		for(j = 0; j < i; ++j){
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
+		}
+		if(l != -1)
+			for(j = 0; j < i; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
+		for(j = i+1; j < N; ++j){
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
+		}
+		if(l != -1)
+			for(j = i+1; j < N; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
+		if(l != -1)
+			A = swap_lines(A, N, i, l);
+		l = -1;
+	}
+	
+	printf("\nConditioned Matrix:\n");
+	print_matrix(A, N);
+	
+	if(line_criterion(A, N)){ 
+		printf("\nThis system does not converge.\n");
+		return NULL; 
+	}
+	
 	for(i = 0; i < N; ++i) b[i] = A[i][N];
 	
 	printf("\n");
@@ -415,12 +493,19 @@ double* gauss_elimination(double **A, int N){
 	
 	for(i = 0; i < N; ++i){
 		for(j = 0; j < i; ++j){
-			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0) l = j;
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
 		}
+		if(l != -1)
+			for(j = 0; j < i; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
 		for(j = i+1; j < N; ++j){
-			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0) l = j;
+			if(fabs(A[j][i]) > fabs(A[i][i]) && A[j][i] != 0){ l = j; break; }
 		}
-
+		if(l != -1)
+			for(j = i+1; j < N; ++j){
+				if(fabs(A[j][i]) > fabs(A[l][i])) l = j;
+			}
 		if(l != -1)
 			A = swap_lines(A, N, i, l);
 		l = -1;
@@ -435,11 +520,11 @@ double* gauss_elimination(double **A, int N){
 		for(i = k + 1; i < N; ++i){
 			if(A[k][k] == 0){
 				for(j = 0; j < k; ++j){
-					if(A[j][k] != 0 && fabs(A[j][k]) > fabs(A[l][k])) l = j;
+					if(l != -1 && A[j][k] != 0 && fabs(A[j][k]) > fabs(A[l][k])) l = j;
 				}
 				
 				for(j = k+1; j < N; ++j){
-					if(A[j][k] != 0 && fabs(A[j][k]) > fabs(A[l][k])) l = j;
+					if(l != -1 && A[j][k] != 0 && fabs(A[j][k]) > fabs(A[l][k])) l = j;
 				}
 				
 				printf("%d %d\n", k, l);
